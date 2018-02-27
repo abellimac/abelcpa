d5cd0caaaae52b0e2214e3123e670a9571c59e2c
alima <alima@globalnexo.com>
2018-02-26 21:26:02 -0400

    con codeigniter

--- CPA con ERIC_files/in.php
+++
@@ -1,3 +0,0 @@
-// static34
-if( window._genericStats ) _genericStats.set_cookie('_jsuid', '2737686364');if( window._genericStats ) _genericStats.set_cookie('heatmaps_g2g_101014277', 'yes', 3600 );
-// exit trax0r
